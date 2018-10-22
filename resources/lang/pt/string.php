135a9ff0004fd96ccb7c2a61f5dcb59103e1f2db
FOGAS\jorge.almeida <jorgewra@gmail.com>
2018-10-22 08:29:01 -0400

    pt-BR

---
+++ resources/lang/pt/string.php
@@ -0,0 +1,64 @@
+<?php
+
+	return 	[ 'loginIndex' =>[
+                'tituloLogin' => 'Autenticação',
+                'campoEmail' => 'Seu e-mail',
+                'campoSenha' => 'Sua senha',
+                'btEntar'=> 'Entar',
+                'linkRegistro'=> 'Novo cadastro',
+                'linkRecuperarSenha'=>'Recuperar Senha'
+            ],
+             'pageRegistro' =>[
+                'titulo' => 'Novo cadastro',
+                'campoEmail' => 'Seu e-mail',
+                'campoNome' => 'Seu nome',
+                'campoSobreNome' => 'Seu sobre nome',
+                'campoSenha' => 'Sua senha',
+                'campoRepitirSenha' => 'Repita sua senha',
+                'btSalvar'=> 'Salvar',
+                'linkLogin'=> 'Voltar ao Login',
+                'linkRecuperarSenha'=>'Recuperar Senha'
+             ],
+             'pageRecuperarSenha' =>[
+                'titulo' => 'Recuperar Senha!',
+                'mensagemInfo' => 'Você esqueceu sua senha?',
+                'mensagemInstrucao' => 'Informe seu endereço de e-mail de acesso e enviaremos uma nova senha!',
+                'campoEmail' => 'Informe seu e-mail',
+                'btEnviar'=> 'Enviar nova senha',
+                'linkRegistro'=> 'Novo cadastro',
+                'linkLogin'=> 'Voltar ao Login',
+             ],
+             'pageHome' =>[
+                'menu1'=>'Inicio',
+                'menu2'=>'Registro de OP', 
+                'menu3'=>'Produtos',
+                'menu4'=>'Processos',
+                'headPesquisa' =>'Pesquisa OP..',
+                'headConfig' =>'Configurações',
+                'headSair' =>'Sair',
+                'tituloInicio'=>'Lista de Ordens de Produção (OP)',
+                'tabOp1'=>'N. Op',
+                'tabOp2'=>'Descrição',
+                'tabOp3'=>'Data',
+                'tabOp4'=>'Total Peças',
+                'tabOp5'=>'Processo',
+                'tabOp6'=>'Status',
+                'tabOp7'=>'Prioridade',
+                'tabOp8'=>'Detalhes',
+                'footOP'=>'Ultimas atulizações ',    
+                'btDetail'=>'Editar',    
+                'btCriar'=>'Criar Novo',
+                'btSalvar'=>'Salvar',
+                'status'=>['Normal','Alta','Média'],
+                'tituloProduto'=>'Lista de Produtos',
+                'tabProduto'=>['Código','Nome','Data Criação'],
+                'btGerarOp'=>'Gerar OP',
+                'situacao'=>[['st'=>'Enable','key'=>'1'],['st'=>'Disable','key'=>'2']],
+                'tituloCadastroProd'=>'Cadastro de Produto', 
+                'campo1'=>'Código do Produto',
+                'campo2'=>'Nome do Produto',
+                'campo3'=>'Descricão do Produto',
+
+                ]   
+            ];
+        
